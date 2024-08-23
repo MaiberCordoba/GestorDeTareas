7767de72a99ceb36db487fb9195a8e9a53ce9351
MaiberCordoba <cordobamaiber@gmail.com>
2024-08-23 11:32:22 -0500

    finalizando proyecto

--- gestor/models.py
+++ gestor/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Tarea(models.model):
-    estados = [{'terminado':'tarea finalizada'},
-              {'icompleto':'tareas incompleta'}]
+class Tarea(models.Model):
+    estados = [('terminado','tarea finalizada'),
+              ('incompleto','tareas incompleta')]
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField(max_length=500)
-    estado = models.CharField(choices=estados, default='icompleto')
+    estado = models.CharField(max_length=100,choices=estados, default='incompleto')
     def __str__(self):
         return self.titulo
